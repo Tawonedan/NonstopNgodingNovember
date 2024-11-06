27682421733ac5c3db552670d64408b1589fd548
Tawonedan <Lebahke1@gmail.com>
2024-11-06 23:56:47 +0700

    Day 6: Login sederhana dengan PHP dan SQL

---
+++ Day6/berhasil_login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: Login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Berhasil Login</title>
+</head>
+<body>
+    <div class="container-logout">
+        <form action="logout.php" method="POST" class="login-email">
+            <h1>Selamat datang, <?php echo $_SESSION['username']; ?>!</h1>
+            <div class="input-group">
+                <button type="submit" class="btn">Logout</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
