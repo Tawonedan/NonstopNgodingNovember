c271183c6e243056af40a40ebb5a96fbc46cdb30
Tawonedan <Lebahke1@gmail.com>
2024-11-13 23:46:47 +0700

    Day 13: menggunakan real time currency dari DB mySQL dengan PHP dan JS
    Meski sekarang currency mengambil dari DB, tampilan currency perlu relog untuk terlihat secara langsung

---
+++ Day13/index.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+include 'config.php';
+
+if (!isset($_SESSION['username'])) {
+    header("Location: Login.php");
+    exit();
+}
+
+$randomNumbers = [];
+for ($i = 0; $i < 10; $i++) {
+    $randomNumbers[] = rand(1, 10);
+}
+
+$results = [];
+
+$currency = 0;
+
+$user_id = $_SESSION['user_id'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprei Gratis</title>
+    <link rel="stylesheet" href="index.css">
+
+</head>
+<body>
+    <div class="menu">
+        <div class="main-menu">
+            <form id="form" action="" method="POST">
+                <h3>Mana sprei gratis yang kau janjikan itu hah?</h3>
+                 <input type="hidden" name="submit-form">
+            </form>
+
+            <button onclick="submitForm()" >Aku mau sprei gratis</button>
+            
+            <div class="addButton">
+                <br>
+                <button type="button" onclick="increaseValue()">Tambah Bhinneka point</button>
+                <p>Bhinneka point saat ini: <span id="displayValue"><?php echo $_SESSION['currency']; ?></span></p>
+
+                <form id="add_credit" action="" method="POST">
+                     <input type="hidden" name="add">
+                </form>
+
+                <?php 
+                if (isset($_POST['add'])) {
+                    $sql = "UPDATE users SET bpoint = bpoint + 1000 WHERE id = '$user_id'" ;
+                    $result = $conn->query($sql);
+                }
+                
+                ?>
+            </div>
+        </div>
+
+        <div class="user-menu">
+        <form action="logout.php" method="POST" class="login-email">
+            <h1>Selamat datang, <?php echo $_SESSION['username']; ?>!</h1>
+            <div class="input-group">
+                <button type="submit" class="btn">Logout</button>
+            </div>
+        </form>
+    </div>
+    </div>
+
+    <div class="result">
+
+        <div class="gallery">
+            <?php
+            if (isset($_POST['submit-form'])) {
+                $updateQuery = "UPDATE users SET bpoint = bpoint - 1000 WHERE id = '$user_id'";
+                $conn->query($updateQuery);
+                foreach ($randomNumbers as $number) {
+                    $sql = "SELECT * FROM items WHERE id = $number";
+                    $result = $conn->query($sql);
+                    
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            $results[] = $row;
+                        }
+                    }
+                }
+                
+                foreach ($results as $row) {
+            ?>
+
+        <div class="card-list">
+            <div class="card">
+                <img src="img/<?php echo $row['image'];?>" alt="">
+                <div class="card-container">
+                    <h4><b><?php echo $row['name'];?></b></h4>
+                    <p class="rarity-<?php echo $row['rarity'];?>"><?php echo $row['rarity'];?></p>
+                </div>
+            </div>
+        </div>
+        <?php } } ?>
+    </div>
+    
+    </div>
+</body>
+<script src="index.js"></script>
+</html>
+
+<?php
+$conn->close();
+?>
---
+++ Day13/register.php
@@ -0,0 +1,74 @@
+<?php 
+
+include 'config.php';
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = hash('sha256', $_POST['password']);
+    $cpassword = hash('sha256', $_POST['cpassword']);
+
+    if ($password == $cpassword) {
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $result = mysqli_query($conn, $sql);
+        if (!$result -> num_rows > 0) {
+            $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+                echo "<script>alert('Pendaftaran berhasil')</script>";
+                $username = "";
+                $email = "";
+                $_POST['password'] = "";
+                $_POST['cpassword'] = "";
+            } else {
+                echo "<script>alert('Terjadi kesalahan')</script>";
+            }
+        } else {
+            echo "<script>alert('Email sudah ada')</script>";
+        }
+    } else {
+        echo "<script>alert('Password tidak sesuai')</script>";
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            <h3>Daftar Akun</h3>
+            <div class="input-group">
+                <input type="text" name="username" id="" placeholder="Username" required>
+            </div>
+            <div class="input-group">
+                <input type="email" name="email" id="" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" name="password" id="" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <input type="password" name="cpassword" id="" placeholder="Confirm Password" required>
+            </div>
+            <div class="input-group">
+                <button type="submit" name="submit">Daftar</button>
+            </div>
+            <p>Sudah punya akun? <a href="login.php">Login</a></p>
+        </form>
+    </div>
+
+
+</body>
+</html>
