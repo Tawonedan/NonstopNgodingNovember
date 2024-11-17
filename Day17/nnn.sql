9a3c6e0eaa0a97e9ee9ed8e83276223fbf8a7cf3
Tawonedan <Lebahke1@gmail.com>
2024-11-17 22:38:19 +0700

    Day 17: Ganti sprei terpilih saat ini dengan PHP dan SQL

---
+++ Day17/nnn.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 17, 2024 at 04:35 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nnn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `rarity` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `image`, `type`, `rarity`) VALUES
+(1, 'Mindy - B:Lilac', 'mindyBL.jpg', 'Star Dewasa', 'Common'),
+(2, 'Mindy - B:Buttermilk', 'mindyBM.jpg', 'Star Dewasa', 'Common'),
+(3, 'Tranquify - B:Buttermilk', 'tranquifyBM.jpg', 'Star Dewasa', 'Common'),
+(4, 'Dear Daisy - B:Star Lover Ungu', 'daisySLU.jpg', 'Star Dewasa', 'Common'),
+(5, 'Dear Daisy - B:Star Lover Peach', 'daisySLP.jpg', 'Star Dewasa', 'Common'),
+(6, 'Matteo - Gray', 'matteoG.jpg', 'Star Dewasa', 'Uncommon'),
+(7, 'Matteo - Brown', 'matteoB.jpg', 'Star Dewasa', 'Uncommon'),
+(8, 'Candy Floss Berry - B:Blush', 'candyflossB.jpg', 'Star Dewasa', 'Rare'),
+(9, 'Golden Memory - B:Lich Sage', 'goldenmemoryLS.jpg', 'Star Dewasa', 'Rare'),
+(10, 'Maharani', 'maharani.jpg', 'Star Dewasa', 'Epic');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `bpoint` int(11) NOT NULL,
+  `hold` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `bpoint`, `hold`) VALUES
+(2, 'admin', 'admin@gmail.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 0, 1),
+(3, 'cihuyman', 'ahciong@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_items`
+--
+
+CREATE TABLE `user_items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_items`
+--
+
+INSERT INTO `user_items` (`id`, `user_id`, `item_id`) VALUES
+(1, 2, 1),
+(2, 2, 1),
+(3, 2, 2),
+(4, 2, 2),
+(5, 2, 6),
+(8, 3, 8);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hold` (`hold`);
+
+--
+-- Indexes for table `user_items`
+--
+ALTER TABLE `user_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_items`
+--
+ALTER TABLE `user_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_items`
+--
+ALTER TABLE `user_items`
+  ADD CONSTRAINT `user_items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
